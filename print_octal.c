6e4382d3b7e2bb55c6c58fee99f2d339370438c8
eyramakua <eyramagginie@gmail.com>
2023-05-29 13:34:22 -0700

    print_octal.c

---
+++ print_octal.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_octal - prints octal digits
+ *
+ * @args_list: argument list
+ *
+ * Return: number of character printed
+ */
+int print_octal(va_list args_list)
+{
+	int d = 0;
+	int sum = 0;
+	char octals[100];
+	unsigned int num = va_arg(args_list, unsigned int);
+
+	if (num == 0)
+	{
+		_putchar('0');
+		sum++;
+	}
+
+	else
+	{
+		while (num > 0)
+		{
+			octals[d] = (num % 8) + '0';
+			num /= 8;
+			d++;
+		}
+		for (d = d - 1; d > 0; d--)
+		{
+			_putchar(octals[d]);
+			sum++;
+		}
+	}
+	return (sum);
+}
